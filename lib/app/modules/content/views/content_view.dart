c0bfdad4f0edcbb1308b87f4369781237530711b
zufarf <mzufarfainan@gmail.com>
2025-05-08 00:37:09 +0700

    refactor: Simplify content display logic in ContentView and enhance wallpaper controls

--- lib/app/modules/content/views/content_view.dart
+++ lib/app/modules/content/views/content_view.dart
@@ -37,95 +37,34 @@ class ContentView extends GetView<ContentController> {
 
         return Stack(
           children: [
+            //  Background wallpaper
             Positioned.fill(
               child: liveWallpaperController.isWallpaperVisible
                   ? liveWallpaperController.renderWallpaper()
                   : const SizedBox.shrink(),
             ),
-            PageView.builder(
-              controller: _pageController,
-              scrollDirection: Axis.vertical,
-              itemCount: subcategory.contentsCount + 1,
-              onPageChanged: (index) {
-                controller.handlePageChanged(subcategory.id, index);
+            // Konten utama
+            GestureDetector(
+                onTap: audioController.togglePlayPause,
+                onLongPress: liveWallpaperController.toggleWallpaperVisibility,
+                child: PageView.builder(
+                  controller: _pageController,
+                  scrollDirection: Axis.vertical,
+                  itemCount: subcategory.contentsCount + 1,
+                  onPageChanged: (index) {
+                    controller.handlePageChanged(subcategory.id, index);
 
-                // Loop ke awal jika sudah di akhir dan nextCursor == null
-                if (index >= controller.images.length &&
-                    controller.nextCursor == null) {
-                  Future.delayed(Duration(milliseconds: 230), () {
-                    _pageController.jumpToPage(0);
-                  });
-                }
-              },
-              itemBuilder: (context, index) {
-                if (index >= controller.images.length) {
-                  return SizedBox.shrink();
-                }
-
-                return Center(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      // Gambar utama
-                      Container(
-                        constraints: BoxConstraints(
-                          maxHeight: Get.height * 0.65,
-                          maxWidth: double.infinity,
-                        ),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(8),
-                          child: Image.network(
-                              controller.images[index].imageUrls.optimized,
-                              headers: {
-                                'Content-Type': 'application/json',
-                                'Accept': 'application/json',
-                                'Authorization':
-                                    'Bearer ${controller.token.value}',
-                              },
-                              fit: BoxFit.contain,
-                              height: Get.height * 0.65,
-                              loadingBuilder:
-                                  (context, child, loadingProgress) =>
-                                      loadingProgress == null
-                                          ? child
-                                          : Center(
-                                              child: CircularProgressIndicator(
-                                                value: loadingProgress
-                                                            .expectedTotalBytes !=
-                                                        null
-                                                    ? loadingProgress
-                                                            .cumulativeBytesLoaded /
-                                                        (loadingProgress
-                                                                .expectedTotalBytes ??
-                                                            1)
-                                                    : null,
-                                              ),
-                                            ),
-                              errorBuilder: (context, error, stackTrace) {
-                                return const Center(
-                                  child: Icon(
-                                    Icons.error,
-                                    color: Colors.red,
-                                    size: 50,
-                                  ),
-                                );
-                              }),
-                        ),
-                      ),
-
-                      // Watermark di bawah gambar utama
-                      Image.asset(
-                        'assets/images/watermark_icon.png', // Path gambar watermark
-                        fit: BoxFit.contain,
-                        errorBuilder: (context, error, stackTrace) {
-                          return const SizedBox.shrink();
-                        },
-                      )
-                    ],
-                  ),
-                );
-              },
-            ),
+                    // Loop ke awal jika sudah di akhir dan nextCursor == null
+                    if (index >= controller.images.length &&
+                        controller.nextCursor == null) {
+                      Future.delayed(Duration(milliseconds: 230), () {
+                        _pageController.jumpToPage(0);
+                      });
+                    }
+                  },
+                  itemBuilder: (context, index) => _buildContentItem(index),
+                )),
+            // Wallpaper & audio controls
             _buildWallpaperControls(liveWallpaperController, audioController),
           ],
         );
@@ -172,6 +111,70 @@ class ContentView extends GetView<ContentController> {
                   inactiveColor: Colors.white54,
                 ),
               )),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildContentItem(int pageIndex) {
+    if (pageIndex >= controller.images.length) {
+      return SizedBox.shrink();
+    }
+
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          // Gambar utama
+          Container(
+            constraints: BoxConstraints(
+              maxHeight: Get.height * 0.65,
+              maxWidth: double.infinity,
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Image.network(
+                  controller.images[pageIndex].imageUrls.optimized,
+                  headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'Authorization': 'Bearer ${controller.token.value}',
+                  },
+                  fit: BoxFit.contain,
+                  height: Get.height * 0.65,
+                  loadingBuilder: (context, child, loadingProgress) =>
+                      loadingProgress == null
+                          ? child
+                          : Center(
+                              child: CircularProgressIndicator(
+                                value: loadingProgress.expectedTotalBytes !=
+                                        null
+                                    ? loadingProgress.cumulativeBytesLoaded /
+                                        (loadingProgress.expectedTotalBytes ??
+                                            1)
+                                    : null,
+                              ),
+                            ),
+                  errorBuilder: (context, error, stackTrace) {
+                    return const Center(
+                      child: Icon(
+                        Icons.error,
+                        color: Colors.red,
+                        size: 50,
+                      ),
+                    );
+                  }),
+            ),
+          ),
+
+          // Watermark di bawah gambar utama
+          Image.asset(
+            'assets/images/watermark_icon.png', // Path gambar watermark
+            fit: BoxFit.contain,
+            errorBuilder: (context, error, stackTrace) {
+              return const SizedBox.shrink();
+            },
+          )
         ],
       ),
     );
